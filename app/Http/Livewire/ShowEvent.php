1292d903390e8f42f2361cdcaad1773e673c7548
websperodev <shukla.garima15@gmail.com>
2021-02-24 16:58:14 +0530

    removed unwanted files

--- app/Http/Livewire/ShowEvent.php
+++ app/Http/Livewire/ShowEvent.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Event;
-use App\Models\Categories;
 use Livewire\Component;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use DB;
+
+use App\Models\Event;
+use App\Models\Ticket;
+use App\Models\Categories;
 
 class ShowEvent extends Component {
 
@@ -20,17 +21,13 @@ class ShowEvent extends Component {
     public $eventDate = '';
 
     public function mount(Request $req, $searchName = null, $searchCat = null) {
-//        print_r($_POST);
-//        die;
+
         $mytime = Carbon::now();
         $date = $mytime->toDateString();
         $this->allCategories = Categories::all();
         $this->searchName = $searchName;
         $this->searchCat = $searchCat;
 
-//        if(){
-//            
-//        }
         $this->eventDate = $req->input('eventDate');
         $this->searchName = $req->input('eventname');
         if ($this->eventDate != '' && $this->searchName == '') {
@@ -42,24 +39,15 @@ class ShowEvent extends Component {
         } else {
             $allEvents = Event::where('end_date', '>', $date)->orderBy('id', 'desc')->get();
         }
-//        echo '<pre>';
-//        print_r($allEvents); die;
+
         if (!empty($allEvents)) {
             foreach ($allEvents as $key => $event) {
-                $allEvents[$key]['tickets'] = DB::table('tickets')->where(['event_id' => $event['id']])->get();
+                $allEvents[$key]['tickets'] = Ticket::where(['event_id' => $event['id']])->get();
             }
         }
-//         echo '<pre>';
-//        print_r($allEvents); die;
-//        if ($this->searchName != '' && $this->searchCat =='') {
-//            $this->allEvents = Event::where('end_date', '>', $date)->where('name', 'like', '%' . $this->searchName . '%')->orderBy('id', 'desc')->get()->toArray();
-//        }
-//         if ($this->searchName == '' && $this->searchCat !='') {
-//            $this->allEvents = Event::where('end_date', '>', $date)->where('name', 'like', '%' . $this->searchName . '%')->orderBy('id', 'desc')->get()->toArray();
-//        }
+
         $this->allEvents = $allEvents;
-//                 echo '<pre>';  
-//          print_r($this->allEvents);  die;
+
     }
 
     public function searchEvent() {
@@ -84,7 +72,7 @@ class ShowEvent extends Component {
         }
         if (!empty($this->allEvents)) {
             foreach ($this->allEvents as $key => $event) {
-                $this->allEvents[$key]['tickets'] = DB::table('tickets')->where(['event_id' => $event['id']])->get();
+                $this->allEvents[$key]['tickets'] = Ticket::where(['event_id' => $event['id']])->get();
             }
         }
         $this->allEvt = $this->allEvents;
@@ -103,7 +91,7 @@ class ShowEvent extends Component {
                     $this->allEvents = Event::where('end_date', '>', $date)->orderBy('start_date', 'desc')->get()->toArray();
                     break;
                 CASE 'Free':
-                    $tickets = DB::table('tickets')->where('ticket_type', 'free')->distinct()->get();
+                    $tickets = Ticket::where('ticket_type', 'free')->distinct()->get();
 
                     if (!empty($tickets)) {
                         foreach ($tickets as $ticket) {
@@ -112,15 +100,14 @@ class ShowEvent extends Component {
                             if (!empty($eventdat)) {
                                 $event[] = $eventdat;
                             }
-//                                   echo '<pre>';
-//                        print_r($event); die;
+
                         }
                         $this->allEvents = $event;
                     }
 
                     break;
                 CASE 'Paid':
-                    $tickets = DB::table('tickets')->where('ticket_type', 'paid')->distinct()->get();
+                    $tickets = Ticket::where('ticket_type', 'paid')->distinct()->get();
                     if (!empty($tickets)) {
                         foreach ($tickets as $ticket) {
                             $eventdat = Event::where('end_date', '>', $date)->where('id', '=', $ticket->event_id)->orderBy('id', 'desc')->first();
@@ -134,7 +121,7 @@ class ShowEvent extends Component {
             }
             if (!empty($this->allEvents)) {
                 foreach ($this->allEvents as $key => $event) {
-                    $this->allEvents[$key]['tickets'] = DB::table('tickets')->where(['event_id' => $event['id']])->get();
+                    $this->allEvents[$key]['tickets'] = Ticket::where(['event_id' => $event['id']])->get();
                 }
             }
             $this->allEvt = $this->allEvents;
