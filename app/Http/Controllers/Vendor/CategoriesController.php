39495a059ac0695fd279c6f7c3dde9c4a03d73ef
websperodev <websperodev@gmail.com>
2021-02-26 17:49:42 +0530

    category update

--- app/Http/Controllers/Vendor/CategoriesController.php
+++ app/Http/Controllers/Vendor/CategoriesController.php
@@ -2,77 +2,77 @@
 
 namespace App\Http\Controllers\Vendor;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+//use App\Models\Subcategory;
+use App\Models\Categories;
+use Yajra\Datatables\Datatables;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
-
 use Auth;
 use App\Models\Page;
-use App\Models\subcategories;
-use Yajra\Datatables\Datatables;
 
-use Response;
+class SubCategoriesController extends Controller {
 
-class subcategoriesController extends Controller
-{
+    public function __construct() {
+        $this->middleware('auth');
+        $this->middleware('role:vendor');
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         return view('vendor.subcategories.index');
     }
-    public function allsubcategories(Request $request){
-        $allsubcategories = subcategories::orderBy('created_at','desc')->get();
-       
+
+    public function allsubcategories(Request $request) {
+        $allsubcategories = Categories::where('parent_id', '!=', 0)->orderBy('created_at', 'desc')->get();
+
         return DataTables::of($allsubcategories)
-            ->addColumn('name',function($allsubcategories) {
-                return $allsubcategories->name;
-            })    
-            ->addColumn('image',function($allsubcategories) {
-                $img = '-';
-                if($allsubcategories->image != ''){
-                    $img = '<img src="'. url($allsubcategories->image) .'" width="100" height="100">';
-                }
-                
-                return $img;
-            })    
-            ->editColumn('created_at',function($allsubcategories) {
-                if(!empty($allsubcategories->created_at)) {
-                    return getDateOnly($allsubcategories->created_at);
-                }
-                return 'N/A';
-            })
-
-           ->addColumn('action',function($allsubcategories) {
-            if(Auth::user()->id == $allsubcategories['created_by']){
-                $str = '<div class="btn-group dropdown">
+                        ->addColumn('name', function($allsubcategories) {
+                            return $allsubcategories->name;
+                        })
+                        ->addColumn('image', function($allsubcategories) {
+                            $img = '-';
+                            if ($allsubcategories->image != '') {
+                                $img = '<img src="' . url($allsubcategories->image) . '" width="100" height="100">';
+                            }
+                            return $img;
+                        })
+                        ->editColumn('created_at', function($allsubcategories) {
+                            if (!empty($allsubcategories->created_at)) {
+                                return getDateOnly($allsubcategories->created_at);
+                            }
+                            return 'N/A';
+                        })
+                        ->addColumn('action', function($allsubcategories) {
+                            if (Auth::user()->id == $allsubcategories['created_by']) {
+                                $str = '<div class="btn-group dropdown">
                 <a href="javascript: void(0);" class="table-action-btn dropdown-toggle arrow-none btn btn-light btn-sm" data-toggle="dropdown" aria-expanded="false"><i class="mdi mdi-dots-horizontal"></i></a>
-                <div class="dropdown-menu dropdown-menu-right"><a data-toggle="tooltip" data-placement="top" title="Edit" class="dropdown-item" href="'.route('event-subcategories.edit', $allsubcategories->id ).'"  ><i class="mdi mdi-pencil mr-1 text-muted font-18 vertical-middle"></i> Edit Category</a>';
-                
-                $str .= '<a data-toggle="tooltip" data-placement="top" title="Delete" class="dropdown-item"   onclick="deleteCategory(this,'.$allsubcategories['id'].')" href="javascript:void(0);" ><i class="mdi mdi-delete mr-1 text-muted font-18 vertical-middle"></i> Delete Category</a>';
-                $str .= '</div></div>';
-                return $str;
-            }else{
-                return '-';
-            }
-            }) 
-            ->rawColumns(['name','image','created_at', 'action'])
-            ->make(true);         
-    }
-   
+                <div class="dropdown-menu dropdown-menu-right"><a data-toggle="tooltip" data-placement="top" title="Edit" class="dropdown-item" href="' . route('subcategories.edit', $allsubcategories->id) . '"  ><i class="mdi mdi-pencil mr-1 text-muted font-18 vertical-middle"></i> Edit Category</a>';
 
+                                $str .= '<a data-toggle="tooltip" data-placement="top" title="Delete" class="dropdown-item"   onclick="deleteSubCategory(this,' . $allsubcategories->id . ')" href="javascript:void(0);" ><i class="mdi mdi-delete mr-1 text-muted font-18 vertical-middle"></i> Delete Category</a>';
+                                $str .= '</div></div>';
+                                return $str;
+                            } else {
+                                return '-';
+                            }
+                        })
+                        ->rawColumns(['name', 'image', 'created_at', 'action'])
+                        ->make(true);
+    }
 
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        return view('vendor.subcategories.add');
+    public function create() {
+        $categories = Categories::where('parent_id', 0)->get();
+        return view('vendor.subcategories.add', compact('categories'));
     }
 
     /**
@@ -81,34 +81,36 @@ class subcategoriesController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    
-    public function store(Request $request)
-    { 
-       
-        try{
-             $validator = Validator::make($request->all(), [
-                'category_name'   => 'required',
+    public function store(Request $request) {
+//echo '<pre>';
+//print_r($request->all()); die;
+        try {
+            $validator = Validator::make($request->all(), [
+                        'category_id' => 'required',
+                        'name' => 'required',
             ]);
 
-            if ($validator->fails())
-            {  
+            if ($validator->fails()) {
                 return redirect()->back()->withErrors($validator);
             }
-            try{
+            try {
                 $user = Auth::user();
                 $data = $request->all();
-                $existing = subcategories::where('name', $data['category_name'])->count();
-                if($existing > 0){
+                $existing = Categories::where('name', $data['name'])->where('parent_id', '!=', '0')->count();
+//                echo '<pre>';
+//                print_r($existing); die;
+                if ($existing > 0) {
                     $request->session()->flash('message.level', 'danger');
-                    $request->session()->flash('message.text', 'Category already exists');
+                    $request->session()->flash('message.text', 'Subcsategory already exists');
                     return redirect()->back();
                 }
 
-                $category = new subcategories;
-                $category->name = $data['category_name'];
-                $category->description = $data['description'];
-                $category->created_by = $user->id;
-                
+                $subcategory = new Categories;
+                $subcategory->parent_id = $data['category_id'];
+                $subcategory->name = $data['name'];
+                $subcategory->description = $data['description'];
+                $subcategory->created_by = $user->id;
+
                 if ($request->hasFile('image_name')) {
                     if ($request->file('image_name')->isValid()) {
                         $validated = $request->validate([
@@ -117,36 +119,35 @@ class subcategoriesController extends Controller
                         ]);
                         $file = request()->file('image_name');
                         $fileName = md5($file->getClientOriginalName() . time()) . "." . $file->getClientOriginalExtension();
-                        $file->move('./uploads/images/', $fileName); 
-                        $img = '/uploads/images/'.$fileName;
-                        $category->image =  $img;
+                        $file->move('./uploads/images/', $fileName);
+                        $img = '/uploads/images/' . $fileName;
+                        $subcategory->image = $img;
                     }
                 }
-                $category->save();
-                
-                if($category->id != ''){
+//                echo '<pre>';
+//print_r($request->all()); die;
+                $subcategory->save();
+
+                if ($subcategory->id != '') {
                     $request->session()->flash('message.level', 'success');
-                    $request->session()->flash('message.text', 'Category Added successfully.');
+                    $request->session()->flash('message.text', 'Subcategory Added successfully.');
                     return redirect()->back();
-                }else{
+                } else {
                     $request->session()->flash('message.level', 'danger');
-                    $request->session()->flash('message.text', 'Category fail.');
+                    $request->session()->flash('message.text', 'Subcategory fail.');
                     return redirect()->back();
                 }
-            }catch (\Exception $e) {
+            } catch (\Exception $e) {
                 $request->session()->flash('message.level', 'danger');
                 $request->session()->flash('message.text', $e->getMessage());
                 return redirect()->back()->withErrors($e->getMessage());
             }
-           
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             $request->session()->flash('message.level', 'danger');
             $request->session()->flash('message.text', $e->getMessage());
             return redirect()->back()->withErrors($e->getMessage());
         }
-        
     }
-
 
     /**
      * Display the specified resource.
@@ -154,21 +155,20 @@ class subcategoriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $iddeleteCategory
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        $category = subcategories::find($id);
-        return view('vendor.subcategories.edit', compact('category'));
+    public function edit($id) {
+        $subcategory = Categories::find($id);
+        $categories = Categories::where('parent_id', 0)->get();
+        return view('vendor.subcategories.edit', compact('subcategory', 'categories'));
     }
 
     /**
@@ -178,34 +178,33 @@ class subcategoriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         $validator = Validator::make($request->all(), [
-                'category_name' => 'required',
+                    'category_id' => 'required',
+                    'name' => 'required',
         ]);
 
-        if ($validator->fails())
-        {   
-            return redirect()->back()->withErrors($validator)->withInput();  
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
         }
-        try{
+        try {
             $data = $request->all();
-            $existing = subcategories::where('name' ,'=', $data['category_name'])->where('id' ,'!=', $data['category_id'])->count();
-            if($existing > 0){
+            $existing = Categories::where('name', '=', $data['name'])->where('parent_id', '!=', 0)->where('id', '!=', $data['id'])->count();
+            if ($existing > 0) {
                 $request->session()->flash('message.level', 'danger');
-                $request->session()->flash('message.text', 'Category already exists');
+                $request->session()->flash('message.text', 'Subcategory already exists');
                 return redirect()->back();
             }
             $user = Auth::user();
 
-            $category = subcategories::find($data['category_id']);
-            $category->name = $data['category_name'];
-            $category->description = $data['description'];
-            $category->created_by = $user->id;            
+            $subcategory = Categories::find($data['id']);
+            $subcategory->parent_id = $data['category_id'];
+            $subcategory->name = $data['name'];
+            $subcategory->description = $data['description'];
+            $subcategory->created_by = $user->id;
             if ($request->hasFile('image_name')) {
                 if ($request->file('image_name')->isValid()) {
-                    if(file_exists(public_path($data['old_file']))){
+                    if (file_exists(public_path($data['old_file']))) {
                         unlink(public_path($data['old_file']));
                         File::delete(public_path($data['old_file']));
                     }
@@ -215,23 +214,23 @@ class subcategoriesController extends Controller
                     ]);
                     $file = request()->file('image_name');
                     $fileName = md5($file->getClientOriginalName() . time()) . "." . $file->getClientOriginalExtension();
-                    $file->move('./uploads/images/', $fileName); 
-                    $img = '/uploads/images/'.$fileName;
-                    $category->image =  $img;
+                    $file->move('./uploads/images/', $fileName);
+                    $img = '/uploads/images/' . $fileName;
+                    $subcategory->image = $img;
                 }
             }
-            $category->update();
-        
-            if($category->id != ''){
+            $subcategory->update();
+
+            if ($subcategory->id != '') {
                 $request->session()->flash('message.level', 'success');
-                $request->session()->flash('message.text', 'Category Updated successfully.');
+                $request->session()->flash('message.text', 'Subcategory Updated successfully.');
                 return redirect()->back();
-            }else{
+            } else {
                 $request->session()->flash('message.level', 'danger');
                 $request->session()->flash('message.text', 'fail.');
                 return redirect()->back();
             }
-        }catch (\Exception $e) {
+        } catch (\Exception $e) {
             $request->session()->flash('message.level', 'danger');
             $request->session()->flash('message.text', $e->getMessage());
             return redirect()->back();
@@ -244,21 +243,19 @@ class subcategoriesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    { 
-        try{
-            $category = subcategories::findOrFail($id);
-            $category->delete();
-           
-            if($category){
-                return Response::json(['success' => true, 'status' => 1, 'message' => "Category has been deleted successfully."]);
-            }else{
+    public function destroy($id) {
+        try {
+            $subcategory = Subcategory::findOrFail($id);
+            $subcategory->delete();
+
+            if ($subcategory) {
+                return Response::json(['success' => true, 'status' => 1, 'message' => "Subcategory has been deleted successfully."]);
+            } else {
                 return Response::json(['success' => false, 'status' => 2, "error" => 'Something went wrong.']);
             }
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             return Response::json(['success' => false, 'status' => 2, "error" => $e->getMessage()]);
-        } 
+        }
     }
 
-   
 }
